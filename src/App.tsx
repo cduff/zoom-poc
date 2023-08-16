b2da1ea4630b4c9556dacb3e1760a7f2ca47c61f
Chris Duff <chris@praxhub.com>
2023-08-16 16:15:34 +1000

    Work...

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import logo from "./images/logo.svg";
+
 export default function App() {
-  return <div>todo</div>;
+  return (
+    <>
+      <div className="headerContainer">
+        <div className="header">
+          <img className="logo" src={logo} alt="logo" />
+        </div>
+      </div>
+      <div>todo</div>
+      <div className="footer">
+        <div className="footerContent">
+          Terms of Use | Privacy Policy | Contact Us
+        </div>
+      </div>
+    </>
+  );
 }
