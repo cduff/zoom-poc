6d7ef7dc8cecb80f49613a9c825f4edcfceab047
Chris Duff <chris@praxhub.com>
2023-08-16 16:50:15 +1000

    Work...

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,21 @@ export default function App() {
           <img className="logo" src={logo} alt="logo" />
         </div>
       </div>
-      <Frame className="frame">todo</Frame>
+      <Frame
+        className="frame"
+        head={
+          <>
+            <script src="https://source.zoom.us/2.15.0/lib/vendor/react.min.js" />
+            <script src="https://source.zoom.us/2.15.0/lib/vendor/react-dom.min.js" />
+            <script src="https://source.zoom.us/2.15.0/lib/vendor/redux.min.js" />
+            <script src="https://source.zoom.us/2.15.0/lib/vendor/redux-thunk.min.js" />
+            <script src="https://source.zoom.us/2.15.0/lib/vendor/lodash.min.js" />
+            <script src="https://source.zoom.us/zoom-meeting-2.15.0.min.js" />
+          </>
+        }
+      >
+        <div>todo</div>
+      </Frame>
       <div className="footer">
         <div className="footerContent">
           Terms of Use | Privacy Policy | Contact Us
