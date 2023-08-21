2a39331ceae8e840782d4a65af4d35058df67ea5
Chris Duff <chris@praxhub.com>
2023-08-21 11:57:50 +1000

    Work...

--- src/App.tsx
+++ src/App.tsx
@@ -60,6 +60,7 @@ export default function App() {
             ZoomMtg.init({
               leaveUrl: "about:blank",
               disablePreview: true,
+              enableHD: true,
               enableFullHD: true,
               meetingInfo: [],
               success: (success: any) => {
