6c6b144daeb64580890cb20bfd6c83d7044f062e
Chris Duff <chris@praxhub.com>
2023-08-23 14:04:22 +1000

    Work...

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ export default function App() {
           <img className="logo" src={logo} alt="logo" />
         </div>
       </div>
+      <div className="spacer" />
       <iframe
         title="Praxhub Zoom"
         className="iframe"
